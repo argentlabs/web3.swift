2ed3034b1de61cf35af899ffff7662a5d105f35a
Miguel Angel Quinones <miguel@miqu.me>
2018-06-07 14:49:06 +0200

    [ADD] Express fixed size data types through protocols in Swift

--- web3swift/src/Contract/Statically Typed/ABIRawType+Static.swift
+++ web3swift/src/Contract/Statically Typed/ABIRawType+Static.swift
@@ -10,18 +10,29 @@ import Foundation
 import BigInt
 
 public protocol ABIType { }
+
 extension String: ABIType { }
 extension Bool: ABIType { }
 extension EthereumAddress: ABIType { }
 extension BigInt: ABIType { }
 extension BigUInt: ABIType { }
 extension Data: ABIType { }
-// TODO (U)Double. Function. Array. Other Int sizes. Fixed binary type (byte<M>)
+// TODO (U)Double. Function. Array. Other Int sizes
 extension Array: ABIType { }
 extension UInt8: ABIType { }
 extension UInt16: ABIType { }
 extension UInt32: ABIType { }
 extension UInt64: ABIType { }
+
+public protocol ABIFixedSizeDataType : ABIType {
+    static var fixedSize: Int { get }
+}
+
+public struct Data32 : ABIFixedSizeDataType {
+    public static var fixedSize: Int {
+        return 32
+    }
+}
 
 extension ABIRawType {
     init?(type: ABIType.Type) {
@@ -36,6 +47,9 @@ extension ABIRawType {
         case is UInt32.Type: self = ABIRawType.FixedUInt(32)
         case is UInt64.Type: self = ABIRawType.FixedUInt(64)
         case is Data.Type: self = ABIRawType.DynamicBytes
+        case is ABIFixedSizeDataType.Type:
+            guard let fixed = type as? ABIFixedSizeDataType.Type else { return nil }
+            self = ABIRawType.FixedBytes(fixed.fixedSize)
         default: return nil
         }
     }
