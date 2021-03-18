b070b3a5c0840f13bb0432c8be5f74c2392d3c73
Miguel Angel Quinones <miguel@miqu.me>
2021-03-18 18:01:30 +0100

    [ADD] Transfer events from

--- web3sTests/ERC20/ERC20Tests.swift
+++ web3sTests/ERC20/ERC20Tests.swift
@@ -79,11 +79,48 @@ class ERC20Tests: XCTestCase {
         waitForExpectations(timeout: 10)
     }
     
-    func testTransferEventsTo() {
+    func testGivenAddressWithInTransfers_ThenGetsTheTransferEvents() {
         let expect = expectation(description: "Get transfer events to")
         
         erc20?.transferEventsTo(recipient: EthereumAddress("0x72e3b687805ef66bf2a1e6d9f03faf8b33f0267a"), fromBlock: .Earliest, toBlock: .Latest, completion: { (error, events) in
             XCTAssert(events!.count > 0)
+            expect.fulfill()
+        })
+        
+        waitForExpectations(timeout: 10)
+    }
+    
+    func testGivenAddressWithoutInTransfers_ThenGetsNoTransferEvents() {
+        let expect = expectation(description: "Get transfer events to")
+        
+        erc20?.transferEventsTo(recipient: EthereumAddress("0x78eac6878f5ef99bf2b12698f03faf8b33f02676"), fromBlock: .Earliest, toBlock: .Latest, completion: { (error, events) in
+            XCTAssertEqual(events?.count, 0)
+            expect.fulfill()
+        })
+        
+        waitForExpectations(timeout: 10)
+    }
+    
+    
+    func testGivenAddressWithOutgoingEvents_ThenGetsTheTransferEvents() {
+        let expect = expectation(description: "Get transfer events to")
+        
+        erc20?.transferEventsFrom(sender: EthereumAddress("0x2FB78FA9842f20bfD515A41C3196C4b368bDbC48"), fromBlock: .Earliest, toBlock: .Latest, completion: { (error, events) in
+            XCTAssertEqual(events?.first?.log.transactionHash, "0xfb6e0d7fdf8f9b97fe9b634cb5abc7041ee47a396191f23425955f9fda008efe")
+            XCTAssertEqual(events?.first?.to, EthereumAddress("0xFe325C1E3396b2285d517B0CE2E3ffA472260Bce"))
+            XCTAssertEqual(events?.first?.value, BigUInt(10).power(18))
+            XCTAssertEqual(events?.first?.log.address, EthereumAddress("0xdb0040451f373949a4be60dcd7b6b8d6e42658b6"))
+            expect.fulfill()
+        })
+        
+        waitForExpectations(timeout: 10)
+    }
+    
+    func testGivenAddressWithoutOutgoingEvents_ThenGetsTheTransferEvents() {
+        let expect = expectation(description: "Get transfer events to")
+        
+        erc20?.transferEventsFrom(sender: EthereumAddress("0x78eac6878f5ef99bf2b12698f03faf8b33f02676"), fromBlock: .Earliest, toBlock: .Latest, completion: { (error, events) in
+            XCTAssertEqual(events?.count, 0)
             expect.fulfill()
         })
         
--- web3swift/src/ERC20/ERC20.swift
+++ web3swift/src/ERC20/ERC20.swift
@@ -16,42 +16,53 @@ public class ERC20 {
         self.client = client
     }
         
-    public func name(tokenContract: EthereumAddress, completion: @escaping((Error?, String?) -> Void)) {
+    public func name(tokenContract: EthereumAddress,
+                     completion: @escaping((Error?, String?) -> Void)) {
         let function = ERC20Functions.name(contract: tokenContract)
         function.call(withClient: self.client, responseType: ERC20Responses.nameResponse.self) { (error, nameResponse) in
             return completion(error, nameResponse?.value)
         }
     }
     
-    public func symbol(tokenContract: EthereumAddress, completion: @escaping((Error?, String?) -> Void)) {
+    public func symbol(tokenContract: EthereumAddress,
+                       completion: @escaping((Error?, String?) -> Void)) {
         let function = ERC20Functions.symbol(contract: tokenContract)
         function.call(withClient: self.client, responseType: ERC20Responses.symbolResponse.self) { (error, symbolResponse) in
             return completion(error, symbolResponse?.value)
         }
     }
     
-    public func decimals(tokenContract: EthereumAddress, completion: @escaping((Error?, UInt8?) -> Void)) {
+    public func decimals(tokenContract: EthereumAddress,
+                         completion: @escaping((Error?, UInt8?) -> Void)) {
         let function = ERC20Functions.decimals(contract: tokenContract)
         function.call(withClient: self.client, responseType: ERC20Responses.decimalsResponse.self) { (error, decimalsResponse) in
             return completion(error, decimalsResponse?.value)
         }
     }
     
-    public func balanceOf(tokenContract: EthereumAddress, address: EthereumAddress, completion: @escaping((Error?, BigUInt?) -> Void)) {
+    public func balanceOf(tokenContract: EthereumAddress,
+                          address: EthereumAddress,
+                          completion: @escaping((Error?, BigUInt?) -> Void)) {
         let function = ERC20Functions.balanceOf(contract: tokenContract, account: address)
         function.call(withClient: self.client, responseType: ERC20Responses.balanceResponse.self) { (error, balanceResponse) in
             return completion(error, balanceResponse?.value)
         }
     }
     
-    public func allowance(tokenContract: EthereumAddress, address: EthereumAddress, spender: EthereumAddress, completion: @escaping((Error?, BigUInt?) -> Void)) {
+    public func allowance(tokenContract: EthereumAddress,
+                          address: EthereumAddress,
+                          spender: EthereumAddress,
+                          completion: @escaping((Error?, BigUInt?) -> Void)) {
         let function = ERC20Functions.allowance(contract: tokenContract, owner: address, spender: spender)
         function.call(withClient: self.client, responseType: ERC20Responses.balanceResponse.self) { (error, balanceResponse) in
             return completion(error, balanceResponse?.value)
         }
     }
     
-    public func transferEventsTo(recipient: EthereumAddress, fromBlock: EthereumBlock, toBlock: EthereumBlock, completion: @escaping((Error?, [ERC20Events.Transfer]?) -> Void)) {
+    public func transferEventsTo(recipient: EthereumAddress,
+                                 fromBlock: EthereumBlock,
+                                 toBlock: EthereumBlock,
+                                 completion: @escaping((Error?, [ERC20Events.Transfer]?) -> Void)) {
         
         guard let result = try? ABIEncoder.encode(recipient).bytes, let sig = try? ERC20Events.Transfer.signature() else {
             completion(EthereumSignerError.unknownError, nil)
@@ -72,4 +83,30 @@ public class ERC20 {
             
         }
     }
+    
+    public func transferEventsFrom(sender: EthereumAddress,
+                                   fromBlock: EthereumBlock,
+                                   toBlock: EthereumBlock,
+                                   completion: @escaping((Error?, [ERC20Events.Transfer]?) -> Void)) {
+        
+        guard let result = try? ABIEncoder.encode(sender).bytes, let sig = try? ERC20Events.Transfer.signature() else {
+            completion(EthereumSignerError.unknownError, nil)
+            return
+        }
+        
+        self.client.getEvents(addresses: nil,
+                              topics: [ sig, String(hexFromBytes: result), nil ],
+                              fromBlock: fromBlock,
+                              toBlock: toBlock,
+                              eventTypes: [ERC20Events.Transfer.self]) { (error, events, unprocessedLogs) in
+            
+            if let events = events as? [ERC20Events.Transfer] {
+                return completion(error, events)
+            } else {
+                return completion(error ?? EthereumClientError.decodeIssue, nil)
+            }
+            
+        }
+    }
+
 }
