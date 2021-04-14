181a63fde4f8b9c9f0d8cb6e5ae8d1fcff9b610e
Miguel Angel Quinones <miguel@miqu.me>
2021-04-14 15:42:21 +0200

    [ADD] ERC721 transfer calls

--- web3swift/src/ERC721/ERC721Functions.swift
+++ web3swift/src/ERC721/ERC721Functions.swift
@@ -65,6 +65,112 @@ public enum ERC721Functions {
             try encoder.encode(tokenId)
         }
     }
+    
+    public struct transferFrom: ABIFunction {
+        public static let name = "transferFrom"
+        public let gasPrice: BigUInt?
+        public let gasLimit: BigUInt?
+        public var contract: EthereumAddress
+        public let from: EthereumAddress?
+        
+        public let sender: EthereumAddress
+        public let to: EthereumAddress
+        public let tokenId: BigUInt
+        
+        public init(contract: EthereumAddress,
+                    from: EthereumAddress? = nil,
+                    gasPrice: BigUInt? = nil,
+                    gasLimit: BigUInt? = nil,
+                    sender: EthereumAddress,
+                    to: EthereumAddress,
+                    tokenId: BigUInt) {
+            self.contract = contract
+            self.from = from
+            self.gasPrice = gasPrice
+            self.gasLimit = gasLimit
+            self.sender = sender
+            self.to = to
+            self.tokenId = tokenId
+        }
+        
+        public func encode(to encoder: ABIFunctionEncoder) throws {
+            try encoder.encode(sender)
+            try encoder.encode(to)
+            try encoder.encode(tokenId)
+        }
+    }
+    
+    public struct safeTransferFrom: ABIFunction {
+        public static let name = "safeTransferFrom"
+        public let gasPrice: BigUInt?
+        public let gasLimit: BigUInt?
+        public var contract: EthereumAddress
+        public let from: EthereumAddress?
+        
+        public let sender: EthereumAddress
+        public let to: EthereumAddress
+        public let tokenId: BigUInt
+        
+        public init(contract: EthereumAddress,
+                    from: EthereumAddress? = nil,
+                    gasPrice: BigUInt? = nil,
+                    gasLimit: BigUInt? = nil,
+                    sender: EthereumAddress,
+                    to: EthereumAddress,
+                    tokenId: BigUInt) {
+            self.contract = contract
+            self.from = from
+            self.gasPrice = gasPrice
+            self.gasLimit = gasLimit
+            self.sender = sender
+            self.to = to
+            self.tokenId = tokenId
+        }
+        
+        public func encode(to encoder: ABIFunctionEncoder) throws {
+            try encoder.encode(sender)
+            try encoder.encode(to)
+            try encoder.encode(tokenId)
+        }
+    }
+    
+    public struct safeTransferFromAndData: ABIFunction {
+        public static let name = "safeTransferFrom"
+        public let gasPrice: BigUInt?
+        public let gasLimit: BigUInt?
+        public var contract: EthereumAddress
+        public let from: EthereumAddress?
+        
+        public let sender: EthereumAddress
+        public let to: EthereumAddress
+        public let tokenId: BigUInt
+        public let data: Data
+        
+        public init(contract: EthereumAddress,
+                    from: EthereumAddress? = nil,
+                    gasPrice: BigUInt? = nil,
+                    gasLimit: BigUInt? = nil,
+                    sender: EthereumAddress,
+                    to: EthereumAddress,
+                    tokenId: BigUInt,
+                    data: Data) {
+            self.contract = contract
+            self.from = from
+            self.gasPrice = gasPrice
+            self.gasLimit = gasLimit
+            self.sender = sender
+            self.to = to
+            self.tokenId = tokenId
+            self.data = data
+        }
+        
+        public func encode(to encoder: ABIFunctionEncoder) throws {
+            try encoder.encode(sender)
+            try encoder.encode(to)
+            try encoder.encode(tokenId)
+            try encoder.encode(data)
+        }
+    }
 }
 
 public enum ERC721MetadataFunctions {
