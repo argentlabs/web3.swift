47992bad8b30f90d5578736521577c07d789dccd
Ronald Mannak <ronald@starlingprotocol.com>
2021-08-15 22:59:19 -0700

    Update ERC20 to async

---
+++ web3sTests/ERC20/ERC20AsyncTests.swift
@@ -0,0 +1,80 @@
+//
+//  ERC20Tests.swift
+//  web3swiftTests
+//
+//  Created by Matt Marshall on 13/04/2018.
+//  Copyright © 2018 Argent Labs Limited. All rights reserved.
+//
+
+import XCTest
+import BigInt
+@testable import web3
+
+class ERC20AsyncTests: XCTestCase {
+    var client: EthereumClient?
+    var erc20: ERC20?
+    let testContractAddress = EthereumAddress(TestConfig.erc20Contract)
+    
+    override func setUp() {
+        super.setUp()
+        self.client = EthereumClient(url: URL(string: TestConfig.clientUrl)!)
+        self.erc20 = ERC20(client: client!)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testName() async throws {
+        let name = try await erc20!.name(tokenContract: self.testContractAddress)
+        XCTAssertEqual(name, "BokkyPooBah Test Token")
+    }
+    
+    func testNonZeroDecimals() async throws {
+        let decimals = try await erc20!.decimals(tokenContract: self.testContractAddress)
+        XCTAssertEqual(decimals, 18)
+    }
+    
+    func testZeroDecimals() async throws {
+        let decimals = try await erc20!.decimals(tokenContract: EthereumAddress("0x40dd3ac2481960cf34d96e647dd0bc52a1f03f52"))
+        XCTAssertEqual(decimals, 0)
+    }
+    
+    func testSymbol() async throws {
+        let symbol = try await erc20!.symbol(tokenContract: self.testContractAddress)
+        XCTAssertEqual(symbol, "BOKKY")
+    }
+    
+    func testTransferRawEvent() async throws {
+        let result = try! ABIEncoder.encode(EthereumAddress("0x72e3b687805ef66bf2a1e6d9f03faf8b33f0267a"))
+        let sig = try! ERC20Events.Transfer.signature()
+        let topics = [ sig, result.hexString]
+    
+        let (events, unprocessed) = try await self.client!.getEvents(addresses: nil, topics: topics, fromBlock: .earliest, toBlock: .latest, eventTypes: [ERC20Events.Transfer.self])
+        XCTAssert(events.count > 0)
+    }
+    
+    func testGivenAddressWithInTransfers_ThenGetsTheTransferEvents() async throws {
+        let events = try await erc20!.transferEventsTo(recipient: EthereumAddress("0x72e3b687805ef66bf2a1e6d9f03faf8b33f0267a"), fromBlock: .earliest, toBlock: .latest)
+        XCTAssert(events.count > 0)
+    }
+    
+    func testGivenAddressWithoutInTransfers_ThenGetsNoTransferEvents() async throws {
+        let events = try await erc20!.transferEventsTo(recipient: EthereumAddress("0x78eac6878f5ef99bf2b12698f03faf8b33f02676"), fromBlock: .earliest, toBlock: .latest)
+        XCTAssertEqual(events.count, 0)
+    }
+        
+    func testGivenAddressWithOutgoingEvents_ThenGetsTheTransferEvents() async throws {
+        let events = try await erc20!.transferEventsFrom(sender: EthereumAddress("0x2FB78FA9842f20bfD515A41C3196C4b368bDbC48"), fromBlock: .earliest, toBlock: .latest)
+        XCTAssertEqual(events.first?.log.transactionHash, "0xfb6e0d7fdf8f9b97fe9b634cb5abc7041ee47a396191f23425955f9fda008efe")
+        XCTAssertEqual(events.first?.to, EthereumAddress("0xFe325C1E3396b2285d517B0CE2E3ffA472260Bce"))
+        XCTAssertEqual(events.first?.value, BigUInt(10).power(18))
+        XCTAssertEqual(events.first?.log.address, EthereumAddress("0xdb0040451f373949a4be60dcd7b6b8d6e42658b6"))
+    }
+    
+    func testGivenAddressWithoutOutgoingEvents_ThenGetsTheTransferEvents() async throws {
+        let events = try await erc20!.transferEventsFrom(sender: EthereumAddress("0x78eac6878f5ef99bf2b12698f03faf8b33f02676"), fromBlock: .earliest, toBlock: .latest)
+        XCTAssertEqual(events.count, 0)
+    }
+    
+}
--- web3swift/src/ERC20/ERC20.swift
+++ web3swift/src/ERC20/ERC20.swift
@@ -16,97 +16,165 @@ public class ERC20 {
         self.client = client
     }
         
+    @available(*, deprecated, message: "Prefer async alternative instead")
     public func name(tokenContract: EthereumAddress,
                      completion: @escaping((Error?, String?) -> Void)) {
-        let function = ERC20Functions.name(contract: tokenContract)
-        function.call(withClient: self.client, responseType: ERC20Responses.nameResponse.self) { (error, nameResponse) in
-            return completion(error, nameResponse?.value)
+        async {
+            do {
+                let result = try await name(tokenContract: tokenContract)
+                completion(nil, result)
+            } catch {
+                completion(error, nil)
+            }
         }
     }
     
+    public func name(tokenContract: EthereumAddress) async throws -> String {
+        let function = ERC20Functions.name(contract: tokenContract)
+        let nameResponse = try await function.call(withClient: self.client, responseType: ERC20Responses.nameResponse.self)
+        return nameResponse.value
+    }
+    
+    @available(*, deprecated, message: "Prefer async alternative instead")
     public func symbol(tokenContract: EthereumAddress,
                        completion: @escaping((Error?, String?) -> Void)) {
-        let function = ERC20Functions.symbol(contract: tokenContract)
-        function.call(withClient: self.client, responseType: ERC20Responses.symbolResponse.self) { (error, symbolResponse) in
-            return completion(error, symbolResponse?.value)
+        async {
+            do {
+                let result = try await symbol(tokenContract: tokenContract)
+                completion(nil, result)
+            } catch {
+                completion(error, nil)
+            }
         }
     }
     
+    public func symbol(tokenContract: EthereumAddress) async throws -> String {
+        let function = ERC20Functions.symbol(contract: tokenContract)
+        let symbolResponse = try await function.call(withClient: self.client, responseType: ERC20Responses.symbolResponse.self)
+        return symbolResponse.value
+    }
+    
+    @available(*, deprecated, message: "Prefer async alternative instead")
     public func decimals(tokenContract: EthereumAddress,
                          completion: @escaping((Error?, UInt8?) -> Void)) {
+        async {
+            do {
+                let result = try await decimals(tokenContract: tokenContract)
+                completion(nil, result)
+            } catch {
+                completion(error, nil)
+            }
+        }
+    }
+    
+    public func decimals(tokenContract: EthereumAddress) async throws -> UInt8? {
         let function = ERC20Functions.decimals(contract: tokenContract)
-        function.call(withClient: self.client, responseType: ERC20Responses.decimalsResponse.self) { (error, decimalsResponse) in
-            return completion(error, decimalsResponse?.value)
+        let decimalsResponse = try await function.call(withClient: self.client, responseType: ERC20Responses.decimalsResponse.self)
+        return decimalsResponse.value
+    }
+    
+    @available(*, deprecated, message: "Prefer async alternative instead")
+    public func balanceOf(tokenContract: EthereumAddress,
+                          address: EthereumAddress,
+                          completion: @escaping((Error?, BigUInt?) -> Void)) {
+        async {
+            do {
+                let result = try await balanceOf(tokenContract: tokenContract, address: address)
+                completion(nil, result)
+            } catch {
+                completion(error, nil)
+            }
         }
     }
     
     public func balanceOf(tokenContract: EthereumAddress,
-                          address: EthereumAddress,
-                          completion: @escaping((Error?, BigUInt?) -> Void)) {
+                          address: EthereumAddress) async throws -> BigUInt {
         let function = ERC20Functions.balanceOf(contract: tokenContract, account: address)
-        function.call(withClient: self.client, responseType: ERC20Responses.balanceResponse.self) { (error, balanceResponse) in
-            return completion(error, balanceResponse?.value)
+        let balanceResponse = try await function.call(withClient: self.client, responseType: ERC20Responses.balanceResponse.self)
+        return balanceResponse.value
+    }
+    
+    @available(*, deprecated, message: "Prefer async alternative instead")
+    public func allowance(tokenContract: EthereumAddress,
+                          address: EthereumAddress,
+                          spender: EthereumAddress,
+                          completion: @escaping((Error?, BigUInt?) -> Void)) {
+        async {
+            do {
+                let result = try await allowance(tokenContract: tokenContract, address: address, spender: spender)
+                completion(nil, result)
+            } catch {
+                completion(error, nil)
+            }
         }
     }
     
     public func allowance(tokenContract: EthereumAddress,
                           address: EthereumAddress,
-                          spender: EthereumAddress,
-                          completion: @escaping((Error?, BigUInt?) -> Void)) {
+                          spender: EthereumAddress) async throws -> BigUInt {
         let function = ERC20Functions.allowance(contract: tokenContract, owner: address, spender: spender)
-        function.call(withClient: self.client, responseType: ERC20Responses.balanceResponse.self) { (error, balanceResponse) in
-            return completion(error, balanceResponse?.value)
-        }
+        let balanceResponse = try await function.call(withClient: self.client, responseType: ERC20Responses.balanceResponse.self)
+        return balanceResponse.value
     }
     
+    @available(*, deprecated, message: "Prefer async alternative instead")
     public func transferEventsTo(recipient: EthereumAddress,
                                  fromBlock: EthereumBlock,
                                  toBlock: EthereumBlock,
                                  completion: @escaping((Error?, [ERC20Events.Transfer]?) -> Void)) {
-        
-        guard let result = try? ABIEncoder.encode(recipient).bytes, let sig = try? ERC20Events.Transfer.signature() else {
-            completion(EthereumSignerError.unknownError, nil)
-            return
-        }
-        
-        self.client.getEvents(addresses: nil,
-                              topics: [ sig, nil, String(hexFromBytes: result)],
-                              fromBlock: fromBlock,
-                              toBlock: toBlock,
-                              eventTypes: [ERC20Events.Transfer.self]) { (error, events, unprocessedLogs) in
-            
-            if let events = events as? [ERC20Events.Transfer] {
-                return completion(error, events)
-            } else {
-                return completion(error ?? EthereumClientError.decodeIssue, nil)
+        async {
+            do {
+                let result = try await transferEventsTo(recipient: recipient, fromBlock: fromBlock, toBlock: toBlock)
+                completion(nil, result)
+            } catch {
+                completion(error, nil)
             }
-            
         }
     }
     
+    
+    public func transferEventsTo(recipient: EthereumAddress,
+                                 fromBlock: EthereumBlock = .earliest,
+                                 toBlock: EthereumBlock = .latest) async throws -> [ERC20Events.Transfer] {
+        
+        let result = try ABIEncoder.encode(recipient).bytes
+        let sig = try ERC20Events.Transfer.signature()
+                
+        let (events, _) = try await self.client.getEvents(addresses: nil, topics: [ sig, nil, String(hexFromBytes: result)], fromBlock: fromBlock, toBlock: toBlock, eventTypes: [ERC20Events.Transfer.self])
+        guard let events = events as? [ERC20Events.Transfer] else {
+            throw EthereumClientError.decodeIssue
+        }
+        return events
+    }
+    
+    @available(*, deprecated, message: "Prefer async alternative instead")
     public func transferEventsFrom(sender: EthereumAddress,
                                    fromBlock: EthereumBlock,
                                    toBlock: EthereumBlock,
                                    completion: @escaping((Error?, [ERC20Events.Transfer]?) -> Void)) {
-        
-        guard let result = try? ABIEncoder.encode(sender).bytes, let sig = try? ERC20Events.Transfer.signature() else {
-            completion(EthereumSignerError.unknownError, nil)
-            return
-        }
-        
-        self.client.getEvents(addresses: nil,
-                              topics: [ sig, String(hexFromBytes: result), nil ],
-                              fromBlock: fromBlock,
-                              toBlock: toBlock,
-                              eventTypes: [ERC20Events.Transfer.self]) { (error, events, unprocessedLogs) in
-            
-            if let events = events as? [ERC20Events.Transfer] {
-                return completion(error, events)
-            } else {
-                return completion(error ?? EthereumClientError.decodeIssue, nil)
+        async {
+            do {
+                let result = try await transferEventsFrom(sender: sender, fromBlock: fromBlock, toBlock: toBlock)
+                completion(nil, result)
+            } catch {
+                completion(error, nil)
             }
-            
         }
+    }    
+    
+    public func transferEventsFrom(sender: EthereumAddress,
+                                   fromBlock: EthereumBlock = .earliest,
+                                   toBlock: EthereumBlock = .latest) async throws -> [ERC20Events.Transfer] {
+        
+        let result = try ABIEncoder.encode(sender).bytes
+        let sig = try ERC20Events.Transfer.signature()
+        
+        let (events, unprocessedLogs) = try await self.client.getEvents(addresses: nil, topics: [ sig, String(hexFromBytes: result), nil ], fromBlock: fromBlock, toBlock: toBlock, eventTypes: [ERC20Events.Transfer.self])
+        
+        guard let events = events as? [ERC20Events.Transfer] else {
+            throw EthereumClientError.decodeIssue
+        }
+        return events
     }
 
 }
