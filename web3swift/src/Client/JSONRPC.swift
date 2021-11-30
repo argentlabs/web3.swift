e3f18ac8359cc86352510b3730f1ae6e15a4f45c
Thant Thet <t@ttkz.me>
2021-11-30 20:11:24 +0630

    Make json RPC error result structs public

--- web3swift/src/Client/JSONRPC.swift
+++ web3swift/src/Client/JSONRPC.swift
@@ -16,20 +16,20 @@ struct JSONRPCRequest<T: Encodable>: Encodable {
 }
 
 public struct JSONRPCResult<T: Decodable>: Decodable {
-    var id: Int
-    var jsonrpc: String
-    var result: T
+    public var id: Int
+    public var jsonrpc: String
+    public var result: T
 }
 
 public struct JSONRPCErrorDetail: Decodable {
-    var code: Int
-    var message: String
+    public var code: Int
+    public var message: String
 }
 
 public struct JSONRPCErrorResult: Decodable {
-    var id: Int
-    var jsonrpc: String
-    var error: JSONRPCErrorDetail
+    public var id: Int
+    public var jsonrpc: String
+    public var error: JSONRPCErrorDetail
 }
 
 public enum JSONRPCErrorCode {
