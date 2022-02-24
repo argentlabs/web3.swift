8c084c190a92898f13e0d90c8915f5093065ff04
Dalton Claybrook <daltonclaybrook@gmail.com>
2022-02-24 12:16:59 -0500

    Add private key encryption/decryption functionality to EthereumKeyStorageProtocol extension

---
+++ web3swift/src/Account/EthereumKeyStorage+Password.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public extension EthereumKeyStorageProtocol {
+    func encryptAndStorePrivateKey(key: Data, keystorePassword: String) throws {
+        let encodedKey = try KeystoreUtil.encode(privateKey: key, password: keystorePassword)
+        try storePrivateKey(key: encodedKey)
+    }
+
+    func loadAndDecryptPrivateKey(keystorePassword: String) throws -> Data {
+        let encryptedKey = try loadPrivateKey()
+        return try KeystoreUtil.decode(data: encryptedKey, password: keystorePassword)
+    }
+}
