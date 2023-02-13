e554575a4820d3252d7674fbe02538a606284b95
Miguel Angel Quinones <miguel@miqu.me>
2023-02-13 17:58:39 +0100

    [CHANGE] More ZK2 changes with latest release pre-mainnet (0.13.x)

--- web3sTests/ZKSync/EthereumClient+ZKSyncTests.swift
+++ web3sTests/ZKSync/EthereumClient+ZKSyncTests.swift
@@ -14,11 +14,11 @@ final class EthereumClientZKSyncTests: XCTestCase {
     let eoaAccount = try! EthereumAccount(keyStorage: TestEthereumKeyStorage(privateKey: TestConfig.privateKey))
     let client = ZKSyncClient(url: TestConfig.ZKSync.clientURL)
     var eoaEthTransfer = ZKSyncTransaction(
-        from: .init("0x719561fee351F7aC6560D0302aE415FfBEEc0B51"),
+        from: .init(TestConfig.publicKey),
         to: .init("0x64d0eA4FC60f27E74f1a70Aa6f39D403bBe56793"),
-        value: BigUInt(hex: "0xe8d4a51000")!,
+        value: 100,
         data: Data(),
-        gasLimit: 5000000
+        gasLimit: 300000
     )
     
     func test_GivenEOAAccount_WhenSendETH_ThenSendsCorrectly() async {
