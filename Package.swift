2844188bd266283419257e65b33893bbba9cc19e
Michał Dąbrowski <michal.dabrowski@elympics.ai>
2025-10-10 14:39:42 +0200

    chore: update upgraded dependency name

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt", from: "5.7.0"),
         .package(url: "https://github.com/iwill/generic-json-swift", .upToNextMajor(from: "2.0.2")),
-        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", exact: "0.21.1"),
+        .package(url: "https://github.com/21-DOT-DEV/swift-secp256k1", exact: "0.21.1"),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.16.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.4")
     ],
@@ -29,7 +29,7 @@ let package = Package(
                     .target(name: "Internal_CryptoSwift_PBDKF2"),
                     "BigInt",
                     .product(name: "GenericJSON", package: "generic-json-swift"),
-                    .product(name: "libsecp256k1", package: "secp256k1.swift"),
+                    .product(name: "libsecp256k1", package: "swift-secp256k1"),
                     .product(name: "WebSocketKit", package: "websocket-kit"),
                     .product(name: "Logging", package: "swift-log")
                 ],
